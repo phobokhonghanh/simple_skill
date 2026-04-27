6c27a3b3998c043a90447f56f79ea616473d3ebd
itc <phobokhonghanh>
2026-04-28 00:25:34 +0700

    fix(deploy): add middleware and edgeExternals to open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -11,6 +11,18 @@ const config: OpenNextConfig = {
       queue: 'direct',
     },
   },
+  edgeExternals: ['node:crypto'],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: 'cloudflare-edge',
+      converter: 'edge',
+      proxyExternalRequest: 'fetch',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'direct',
+    },
+  },
 };
 
 export default config;
